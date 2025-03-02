5fe69d60f4aef51f69a2881dc98cc8d3a645dfb7
Stefan <shodgeskluck@gmail.com>
2025-03-01 21:39:48 -0500

    redirecting default route to blogs

--- routes/index.dart
+++ routes/index.dart
@@ -1,6 +1,17 @@
 import 'package:dart_frog/dart_frog.dart';
 
-/// Defaul request handler.
-Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+/// Default request handler, redirects to `/blogs`.
+/// Supports GET requests.
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => await _get(context),
+    _ => Response(statusCode: 405, body: 'Method Not Allowed'),
+  };
+}
+
+Future<Response> _get(RequestContext context) async {
+  return Response(
+    statusCode: 301,
+    headers: {'location': '/blogs'},
+  );
 }
