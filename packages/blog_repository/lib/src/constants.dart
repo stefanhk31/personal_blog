d3fd5fb6d59a6ca4a81a1b1d4b47e13e9e7f62f4
Stefan <shodgeskluck@gmail.com>
2025-12-30 09:32:53 -0500

    formatting

--- packages/blog_repository/lib/src/constants.dart
+++ packages/blog_repository/lib/src/constants.dart
@@ -4,7 +4,8 @@ const defaultMetaTitle = "Stefan's Post-Academic Chronicles";
 
 /// The default meta description for the blog site. Reflects the entire site
 /// content, not the content of an individual post.
-const defaultMetaDescription = 'Exploring my post-academic experiences '
+const defaultMetaDescription =
+    'Exploring my post-academic experiences '
     'in software, the outdoors, and life in general.';
 
 /// The current year, used in the footer of the blog site.
