6dfe51205a43fd46c1faa53077a138ecc7e2d578
Stefan <shodgeskluck@gmail.com>
2025-12-30 11:02:10 -0500

    test newsletter ping

--- blog_update_handler/lib/main.dart
+++ blog_update_handler/lib/main.dart
@@ -1,16 +1,29 @@
 import 'dart:convert';
 import 'dart:io';
 
-void main() {
-  final log = jsonEncode({
+import 'package:http/http.dart';
+
+Future<void> main() async {
+  final regLog = jsonEncode({
     'level': 'info',
-    'message': 'Hello from blog_update_handler!',
+    'message': 'Requesting health check from newsletter.',
     'timestamp': DateTime.now().toIso8601String(),
   });
-  // Logger('BlogUpdateHandler')
-  // .info(log);
-  print(log);
-  
+  print(regLog);
+
+  final uri = Uri.http(
+    'blog_newsletter.railway.internal:8000',
+    '/health_check',
+  );
+  final client = Client();
+  final result = await client.get(uri);
+
+  final resLog = jsonEncode({
+    'level': 'info',
+    'message': 'Response from health check is ${result.statusCode}',
+    'timestamp': DateTime.now().toIso8601String(),
+  });
+  print(resLog);
 
   exit(0);
 }
