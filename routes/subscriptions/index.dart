fcb74329655e51d903bced16f1c303b44dc66d16
Stefan <shodgeskluck@gmail.com>
2026-01-06 08:36:15 -0500

    form data and todo comment

--- routes/subscriptions/index.dart
+++ routes/subscriptions/index.dart
@@ -18,12 +18,20 @@ Future<Response> _post(RequestContext context) async {
 }
 
 Future<Response> _delete(RequestContext context) async {
-  //TODO: this should be query params
-  final body = await context.request.body();
+  //TODO: should this be in a repo?
+  final formData = await context.request.formData();
+  final email = formData.fields['email'];
+
+  if (email == null) {
+    return Response(
+      statusCode: 400,
+      body: '<p>Invalid email</p>',
+    );
+  }
 
   final result = await context
       .read<BlogNewsletterClient>()
-      .removeSubscriber(encodedSubscriberEmail: body);
+      .removeSubscriber(encodedSubscriberEmail: email);
 
   return Response(
     statusCode: result.statusCode,
