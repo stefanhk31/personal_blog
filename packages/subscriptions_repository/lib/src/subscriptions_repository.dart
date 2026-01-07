d644b6d347dc33999a8c0f6c425e70e4555c7a9f
Stefan <shodgeskluck@gmail.com>
2026-01-07 17:00:04 -0500

    scaffolding subscriptions repo

---
+++ packages/subscriptions_repository/lib/src/subscriptions_repository.dart
@@ -0,0 +1,7 @@
+/// {@template subscriptions_repository}
+/// A repository to encapsulate logic managing email subscriptions.
+/// {@endtemplate}
+class SubscriptionsRepository {
+  /// {@macro subscriptions_repository}
+  const SubscriptionsRepository();
+}
